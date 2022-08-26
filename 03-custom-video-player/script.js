ee30742db6bf5d8d4695f3ac1966437e256bc1aa
James Knight <contact@mtn-studio.com>
2022-08-26 14:18:03 -0700

    Another project completed.

--- 03-custom-video-player/script.js
+++ 03-custom-video-player/script.js
@@ -24,12 +24,26 @@ function updatePlayIcon() {
 
 // Update progress & timestamp
 function updateProgress() {
-	return true;
+	progress.value = (video.currentTime / video.duration) * 100;
+
+	// Get minutes
+	let mins = Math.floor(video.currentTime / 60);
+	if (mins < 10) {
+		mins = '0' + String(mins);
+	}
+
+	// Get seconds
+	let secs = Math.floor(video.currentTime % 60);
+	if (secs < 10) {
+		secs = '0' + String(secs);
+	}
+
+	timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-	return true;
+	video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
@@ -43,7 +57,6 @@ video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('timeupdate', updateProgress);
-
 play.addEventListener('click', toggleVideoStatus);
 stop.addEventListener('click', stopVideo);
 progress.addEventListener('change', setVideoProgress);
