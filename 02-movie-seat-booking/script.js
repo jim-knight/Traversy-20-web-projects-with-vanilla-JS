08ace6b186418c6ed05071612b098d776c0d2005
James Knight <contact@mtn-studio.com>
2022-08-25 22:51:53 -0700

    Completed task. Final steps were to populate UI with saved data.

--- 02-movie-seat-booking/script.js
+++ 02-movie-seat-booking/script.js
@@ -3,6 +3,9 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
@@ -27,6 +30,25 @@ function updatedSelectedCount() {
 	count.innerText = selectedSeatsCount;
 	total.innerText = selectedSeatsCount * ticketPrice;
 }
+
+// Get data from localStorage and populate UI
+function populateUI() {
+	const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+	if ((selectedSeats !== null) & (selectedSeats.length > 0)) {
+		seats.forEach((seat, index) => {
+			if (selectedSeats.indexOf(index) > -1) {
+				seat.classList.add('selected');
+			}
+		});
+	}
+
+	const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+	if (selectedMovieIndex !== null) {
+		movieSelect.selectedIndex = selectedMovieIndex;
+	}
+}
+
 // Movie select event
 movieSelect.addEventListener('change', (e) => {
 	ticketPrice = +e.target.value;
@@ -42,3 +64,6 @@ container.addEventListener('click', (e) => {
 		updatedSelectedCount();
 	}
 });
+
+// Initial count and total set
+updatedSelectedCount();
