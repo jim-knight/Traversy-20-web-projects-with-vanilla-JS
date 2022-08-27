df98a6739c3dd3de8cf192b7a85d854830fd7e32
James Knight <contact@mtn-studio.com>
2022-08-26 21:47:25 -0700

    Show millionaires - filter()

--- 05-dom-array-methods/script.js
+++ 05-dom-array-methods/script.js
@@ -37,6 +37,12 @@ function sortByRichest() {
 	updateDOM();
 }
 
+// Filter only millionaires
+function showMillionaires() {
+	data = data.filter((user) => user.money > 1000000);
+	updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
 	data.push(obj);
@@ -68,3 +74,4 @@ function formatMoney(amount) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
