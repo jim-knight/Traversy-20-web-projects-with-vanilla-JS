6c2d10c4c7b9e9a0b5c38dc27af3089efec510c7
James Knight <contact@mtn-studio.com>
2022-08-26 21:53:27 -0700

    Completed last step of project: Calculate wealth - reduce()

--- 05-dom-array-methods/script.js
+++ 05-dom-array-methods/script.js
@@ -43,6 +43,16 @@ function showMillionaires() {
 	updateDOM();
 }
 
+// Calculate total wealth
+function calculateWealth() {
+	const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+	console.log(formatMoney(wealth));
+
+	const wealthEl = document.createElement('div');
+	wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+	main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
 function addData(obj) {
 	data.push(obj);
@@ -75,3 +85,4 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
