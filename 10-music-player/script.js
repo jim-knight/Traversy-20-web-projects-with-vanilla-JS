8b85bac922505d4266ab53b8c05723d5e09fbe34
James Knight <contact@mtn-studio.com>
2022-09-01 14:13:49 -0700

    Added in Next, prev song & progress. Completed project.

--- 10-music-player/script.js
+++ 10-music-player/script.js
@@ -45,6 +45,42 @@ function pauseSong() {
 	audio.pause();
 }
 
+// Previous song
+function prevSong() {
+	songIndex--;
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
+	loadSong(songs[songIndex]);
+	playSong();
+}
+
+// Next song
+function nextSong() {
+	songIndex++;
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
+	loadSong(songs[songIndex]);
+	playSong();
+}
+
+// Update progress bars
+function updateProgress(e) {
+	const { duration, currentTime } = e.srcElement;
+	const progressPercent = (currentTime / duration) * 100;
+	progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
+
+	audio.currentTime = (clickX / width) * duration;
+}
+
 // Event listeners
 playBtn.addEventListener('click', () => {
 	const isPlaying = musicContainer.classList.contains('play');
@@ -54,3 +90,16 @@ playBtn.addEventListener('click', () => {
 		playSong();
 	}
 });
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/ Song update event
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
