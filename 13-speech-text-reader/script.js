5782b499ff2c2e82d3977642932fe475661fb1ff
James Knight <contact@mtn-studio.com>
2022-09-02 14:35:43 -0700

    Completed getting speech voices.

--- 13-speech-text-reader/script.js
+++ 13-speech-text-reader/script.js
@@ -72,3 +72,30 @@ function createBox(item) {
 	// TODO: Speak event
 	main.appendChild(box);
 }
+
+// Store voices
+let voices = [];
+
+function getVoices() {
+	voices = speechSynthesis.getVoices();
+
+	voices.forEach((voice) => {
+		const option = document.createElement('option');
+
+		option.value = voice.name;
+		option.innerText = `${voice.name} ${voice.lang}`;
+
+		voicesSelect.appendChild(option);
+	});
+}
+
+// Voices changes
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'));
+
+// Close button
+closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'));
+
+getVoices();
