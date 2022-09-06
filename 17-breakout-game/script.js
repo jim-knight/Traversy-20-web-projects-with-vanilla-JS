2b2affb3ad6a693ee119ae00c3b931010609363d
James Knight <contact@mtn-studio.com>
2022-09-05 21:29:02 -0700

    Completed moving paddle and wall detection.

--- 17-breakout-game/script.js
+++ 17-breakout-game/script.js
@@ -89,15 +89,58 @@ function drawBricks() {
 	});
 }
 
+// Move paddle on canvas
+function movePaddle() {
+	paddle.x += paddle.dx;
+
+	// Wall detection
+	if (paddle.x + paddle.w > canvas.width) {
+		paddle.x = canvas.width - paddle.w;
+	}
+	if (paddle.x < 0) {
+		paddle.x = 0;
+	}
+}
+
 // Draw everything
 function draw() {
+	// Clear canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
 	drawBall();
 	drawPaddle();
 	drawScore();
 	drawBricks();
 }
 
-draw();
+// Updated canvas drawing and animation
+function update() {
+	movePaddle();
+
+	// Draw everything
+	draw();
+
+	requestAnimationFrame(update);
+}
+update();
+
+// Keydown events
+function keyDown(e) {
+	if (e.key === 'Right' || e.key === 'ArrowRight') {
+		paddle.dx = paddle.speed;
+	} else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+		paddle.dx = -paddle.speed;
+	}
+}
+function keyUp(e) {
+	if (e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' || e.key === 'ArrowLeft') {
+		paddle.dx = 0;
+	}
+}
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 // Rules and close event handler
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
