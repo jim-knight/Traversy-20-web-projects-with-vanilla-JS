a720b5cf8f6d57eb6a5e38d73ac94b6ca810932a
James Knight <contact@mtn-studio.com>
2022-09-08 13:25:52 -0700

    Added checking of order. Project completed.

--- 19-sortable-list/script.js
+++ 19-sortable-list/script.js
@@ -61,11 +61,26 @@ function dragDrop() {
 	this.classList.remove('over');
 }
 
+// Swap list items that are dragged and dropped
 function swapItems(fromIndex, toIndex) {
 	const itemOne = listItems[fromIndex].querySelector('.draggable');
 	const itemTwo = listItems[toIndex].querySelector('.draggable');
 	listItems[fromIndex].appendChild(itemTwo);
 	listItems[toIndex].appendChild(itemOne);
+}
+
+// Check the order of list items
+function checkOrder() {
+	listItems.forEach((listItem, index) => {
+		const personName = listItem.querySelector('.draggable').innerText.trim();
+
+		if (personName !== richestPeople[index]) {
+			listItem.classList.add('wrong');
+		} else {
+			listItem.classList.remove('wrong');
+			listItem.classList.add('right');
+		}
+	});
 }
 
 function addEventListeners() {
@@ -82,3 +97,5 @@ function addEventListeners() {
 		item.addEventListener('dragleave', dragLeave);
 	});
 }
+
+check.addEventListener('click', checkOrder);
