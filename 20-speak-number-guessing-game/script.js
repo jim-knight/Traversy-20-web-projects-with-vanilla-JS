118aca204312314a7d16d8fff5805f58258f2366
James Knight <contact@mtn-studio.com>
2022-09-09 21:35:37 -0700

    Completed code for checking the guessed number. Project and course completed!

--- 20-speak-number-guessing-game/script.js
+++ 20-speak-number-guessing-game/script.js
@@ -14,8 +14,46 @@ recognition.start();
 function onSpeak(e) {
 	const msg = e.results[0][0].transcript;
 
-	// writeMessage(msg);
-	// checkNumber(msg);
+	writeMessage(msg);
+	checkNumber(msg);
+}
+
+// Write what user speaks into mic
+function writeMessage(msg) {
+	msgEl.innerHTML = `
+        <div>You said:</div>
+        <span class="box">${msg}</span>
+    `;
+}
+
+// Check message against number
+function checkNumber(msg) {
+	const num = +msg;
+
+	// Check if valid number
+	if (Number.isNaN(num)) {
+		msgEl.innerHTML += '<div>That is not a valid number</div>';
+		return;
+	}
+
+	// Check in range
+	if (num > 100 || num < 1) {
+		msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+		return;
+	}
+
+	// Check number
+	if (num === randomNum) {
+		document.body.innerHTML = `
+        <h2>Congrats! You have guessed the number!<br><br> 
+        It was ${num}</h2>
+        <button id="play-again" class="play-again">Play Again</button>
+        `;
+	} else if (num > randomNum) {
+		msgEl.innerHTML += '<div>GO LOWER</div>';
+	} else {
+		msgEl.innerHTML += '<div>GO HIGHER</div>';
+	}
 }
 
 // Generate random number
@@ -25,3 +63,12 @@ function getRandomNumber() {
 
 // Speak result
 recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', (e) => {
+	if (e.target.id == 'play-again') {
+		window.location.reload();
+	}
+});
